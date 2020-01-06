36cde62cb041e6cd33ac98dd4ddacb5d000ef783
fierascu <fierascu@gmail.com>
2020-01-06 17:00:06 +0200

    initial commit

---
+++ flight-time-backend/src/main/java/com/flighttime/flighttimebackend/FlightTimeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flighttime.flighttimebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTimeBackendApplication.class, args);
+	}
+
+}
---
+++ flight-time-backend/src/test/java/com/flighttime/flighttimebackend/FlightTimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flighttime.flighttimebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
