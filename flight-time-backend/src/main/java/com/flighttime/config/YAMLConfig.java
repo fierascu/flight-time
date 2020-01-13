367ed72ea04ebf9e23332bc45d2ded632596f144
fierascu <fierascu@gmail.com>
2020-01-13 07:37:59 +0200

    Added YAMLConfig config with default values for non spring initialization; server.port in application.yml was already used without any special configuration within the spring app.

---
+++ flight-time-backend/src/main/java/com/flighttime/config/YAMLConfig.java
@@ -0,0 +1,46 @@
+package com.flighttime.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class YAMLConfig {
+
+    private static final String DEFAULT_PROTOCOL = "http";
+    private static final String DEFAULT_HOST = "localhost";
+    private static final int DEFAULT_PORT = 8082;
+
+    private static String protocol;
+    private static String host;
+
+    public static String getProtocol() {
+        return protocol == null ? DEFAULT_PROTOCOL : protocol;
+    }
+
+    public static void setProtocol(String protocol) {
+        YAMLConfig.protocol = protocol;
+    }
+
+    public static String getHost() {
+        return host == null ? DEFAULT_HOST : host;
+    }
+
+    public static void setHost(String host) {
+        YAMLConfig.host = host;
+    }
+
+    public static int getPort() {
+        return port == 0 ? DEFAULT_PORT : port;
+    }
+
+    public static void setPort(int port) {
+        YAMLConfig.port = port;
+    }
+
+    private static int port;
+
+}
--- flight-time-backend/src/main/java/com/flighttime/restclient/Client.java
+++ flight-time-backend/src/main/java/com/flighttime/restclient/Client.java
@@ -1,5 +1,8 @@
 package com.flighttime.restclient;
 
+import com.flighttime.config.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -7,45 +10,46 @@ import java.net.URL;
 
 public class Client {
 
-  public static void main(String[] args) throws Exception {
-    String response = getResponse("VIE", "BUD", "flight");
-    System.out.println(response);
+    @Autowired
+    private static YAMLConfig myConfig;
 
-    String response2 = getResponse("VIE", "BUD", "duration");
-    System.out.println(response2);
-  }
+    public static void main(String[] args) throws Exception {
+        String response = getResponse("VIE", "BUD", "flight");
+        System.out.println(response);
 
-  private static String getResponse(String depAirportCode, String arrAirportCode, String endpoint) {
-    try {
-
-      // TODO read from application.yml
-
-      String protocol = "http";
-      String host = "localhost";
-      int port = 8082;
-
-      String url = protocol + "://" + host + ":" + port + "/" + endpoint + "?dep=" + depAirportCode + "&arr=" + arrAirportCode;
-
-      HttpURLConnection httpClient = (HttpURLConnection)new URL(url).openConnection();
-
-      if (httpClient.getResponseCode() == 200) {
-        try (BufferedReader in = new BufferedReader(
-            new InputStreamReader(httpClient.getInputStream()))) {
-
-          StringBuilder response = new StringBuilder();
-          String line;
-
-          while ((line = in.readLine()) != null) {
-            response.append(line);
-          }
-          return response.toString();
-        }
-      }
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
+        String response2 = getResponse("VIE", "BUD", "duration");
+        System.out.println(response2);
     }
 
-    return null;
-  }
+    private static String getResponse(String depAirportCode, String arrAirportCode, String endpoint) {
+        try {
+            String protocol = YAMLConfig.getProtocol();
+            String host = YAMLConfig.getHost();
+            int port = YAMLConfig.getPort();
+
+
+            String url = protocol + "://" + host + ":" + port + "/" + endpoint + "?dep=" + depAirportCode + "&arr=" + arrAirportCode;
+
+            HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();
+
+            if (httpClient.getResponseCode() == 200) {
+                try (BufferedReader in = new BufferedReader(
+                        new InputStreamReader(httpClient.getInputStream()))) {
+
+                    StringBuilder response = new StringBuilder();
+                    String line;
+
+                    while ((line = in.readLine()) != null) {
+                        response.append(line);
+                    }
+                    return response.toString();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
 
