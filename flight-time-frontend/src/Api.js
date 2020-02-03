be035eeb072d60281073663f8def4a08495d7c12
fierascu <fierascu@gmail.com>
2020-02-03 07:42:27 +0200

    frontend: make call work not async, but thru instance and react lifecycle method componentDidUpdate;

--- flight-time-frontend/src/Api.js
+++ flight-time-frontend/src/Api.js
@@ -10,41 +10,60 @@ class Api extends Component {
     this.getDistance();
   }
 
-  getDistance = async () => {
+  componentDidUpdate(prevProps) {
+    if (this.props.dep !== prevProps.dep
+      || this.props.arr !== prevProps.arr) {
+
+      this.getDistance();
+    }
+  }
+
+  getDistance = () => {
     if (this.props.dep === '' && this.props.arr === '') {
       return;
     }
 
-    const url = "http://localhost:8082";
-    const config = {
-      method: 'get',
-      url: url + '/flight',
+    const url = "http://localhost:8082/";
+
+    const instance = axios.create({
+      baseURL: url,
       timeout: 1000,
       headers: { 'Access-Control-Allow-Origin': '*' }
-    }
-
-    let res = await axios(config, {
-      dep: this.props.dep,
-      arr: this.props.arr
     });
 
-    let { data } = res.data;
+    let currentComponent = this;
 
-    console.log('data:' + data);
-    this.setState({ data: data });
+    instance.get('/flight', {
+      params: {
+        dep: this.props.dep,
+        arr: this.props.arr
+      }
+    })
+      .then(function (response) {
+        console.log(response.data);
+        currentComponent.setState({
+          data: response.data
+        })
+        console.log('response:' + response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
   };
+
 
   render() {
     const { data } = this.state;
 
-    return <div><ul>
+    return data === undefined ? null : <div><ul>
       <li key={1}>From <b>{this.props.dep}</b> to <b>{this.props.arr}</b></li>
       <li key={2}>Distance: {data.dist}</li>
       <li key={3}>Duration: {data.duration}</li>
       <li key={4}>Time: {data.durationAsTime}</li>
     </ul></div>;
-
-
   }
 }
 
--- flight-time-frontend/src/App.js
+++ flight-time-frontend/src/App.js
@@ -10,12 +10,10 @@ class App extends Component {
 
     handleSubmit = character => {
         console.log(character)
-        this.setState(
-            {
+        this.setState({
                 arr: character.arr,
                 dep: character.dep,
-            }
-            );
+            });
     }
 
     render() {
@@ -23,10 +21,9 @@ class App extends Component {
         return (
             <div className="container">
             <h3>Flight Time</h3>
-            <p>Easy to use with clean interface application for finding airports</p>
+            <p>Easy to use with clean interface application for calculating flight time between two airports</p>
             <h3>Search</h3>
                 <Form handleSubmit={this.handleSubmit} />
-
                 <h1>Results</h1>
                 <Api arr={arr} dep={dep}/>
             </div>
