3af3af1879e454287563b2d5c4a7161288e1165d
fierascu <fierascu@gmail.com>
2020-02-06 07:38:17 +0200

    frontend: - added new POC for AirportApi and AirportForm: duplicate the inital code; maybe later reuse the same component?

--- flight-time-frontend/src/App.js
+++ flight-time-frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Form from './Form';
+import AirportForm from './AirportForm';
 import Api from './Api';
+import AirportApi from './AirportApi';
 
 class App extends Component {
     state = {
         dep: '',
         arr: '',
-        showAirportData: false
+        showAirportData: false,
+        airportText: ''
     };
 
     handleSubmit = character => {
@@ -14,13 +17,14 @@ class App extends Component {
         this.setState({
             arr: character.arr,
             dep: character.dep,
-            showAirportData: character.showAirportData
+            showAirportData: character.showAirportData,
+            airportText: character.airportText
         });
     }
 
     render() {
-        const { arr, dep, showAirportData } = this.state;
-        return (
+        const { arr, dep, showAirportData, airportText } = this.state;
+        return (<div className="container">Flight Time Calculator| Airport Finder
             <div className="container">
                 <h3>Flight Time</h3>
                 <p>Easy to use with clean interface application for calculating flight time between two airports</p>
@@ -29,6 +33,14 @@ class App extends Component {
                 <h1>Results</h1>
                 <Api arr={arr} dep={dep} showAirportData={showAirportData} />
             </div>
+            <div className="container">
+                <h3>Airport</h3>
+                <p>Easy to use with clean interface application for searching an airports</p>
+                <h3>Search</h3>
+                <AirportForm handleSubmit={this.handleSubmit} />
+                <h1>Results</h1>
+                <AirportApi airportText={airportText} />
+            </div></div>
         );
     }
 }
