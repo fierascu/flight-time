ea5daba9254d5d445a65a444b001b73c988b2f00
fierascu <fierascu@gmail.com>
2020-02-10 07:47:26 +0200

    Release: initial;

---
+++ flight-time-backend/src/main/resources/templates/precache-manifest.4051ef29686498f132e684a1033c71ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ba19714a432984c5f27a14aa5b9c20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18ccb8fb6408c4454af",
+    "url": "/static/css/main.6bfb0a1f.chunk.css"
+  },
+  {
+    "revision": "77f5f7b9f2e69c6f706c",
+    "url": "/static/js/2.1d645e15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1d645e15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d18ccb8fb6408c4454af",
+    "url": "/static/js/main.a9c70367.chunk.js"
+  },
+  {
+    "revision": "8c8d89b5c3445c782b93",
+    "url": "/static/js/runtime-main.595370fb.js"
+  }
+]);
